8de5316da12b0cd9102314f28a6af479369f345e
ayanami420 <123916327+ayanami420@users.noreply.github.com>
2023-02-07 20:53:43 -0500

    Create Lesson 6_2.js

---
+++ Unit 3/Lesson 6/Lesson 6_2.js
@@ -0,0 +1,6 @@
+console.log("Starting my program!");
+setProperty("bigButton", "background-color", "blue");
+setProperty("bigButton", "height", 200);
+setProperty("bigButton", "text", "Now it's a bigger button!");
+console.log("Ending my program!");
+console.log("");
