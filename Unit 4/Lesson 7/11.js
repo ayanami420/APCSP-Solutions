5e95c8f365f89cf97eb3eab3e715214c681a8aa6
ayanami420 <123916327+ayanami420@users.noreply.github.com>
2023-02-07 21:04:15 -0500

    Create 11.js

---
+++ Unit 4/Lesson 7/11.js
@@ -0,0 +1,30 @@
+// Example
+console.log(5%2);
+console.log(1);
+console.log("");
+
+// Write a statement in the first console log command of each set of three // with the MOD operator
+// and the two of the numbers from this list:
+// 1, 2, 2, 2, 3, 4, 7, 10
+// Each number is used exactly once
+
+
+//console.log your answer - check that the two lines match in the console
+console.log(2%10);
+console.log(2);
+console.log("");
+
+// console.log your answer - check that the two lines match in the console
+console.log(3%2);
+console.log(0);
+console.log("");
+
+// console.log your answer - check that the two lines match in the console
+console.log(2%4);
+console.log(2);
+console.log("");
+
+// console.log your answer - check that the two lines match in the console
+console.log(7%1);
+console.log(0);
+console.log("");
