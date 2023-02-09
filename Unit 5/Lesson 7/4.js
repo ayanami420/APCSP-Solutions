8776a808ed026584971b8a4fd796d46e4231bb55
ayanami420 <123916327+ayanami420@users.noreply.github.com>
2023-02-08 20:04:02 -0500

    Create 4.js

---
+++ Unit 5/Lesson 7/4.js
@@ -0,0 +1,14 @@
+// Create and assign variable based on user input
+var counter = promptNum("Enter a number");
+
+if (counter > 0){
+  while(counter){
+  console.log(counter);
+  counter--;
+  //then decrease the value of the counter by 1 each time
+}
+}
+
+
+//TO DO #3: Add code to print "Blastoff!" after the loop is done running
+console.log("BLASTOFF!");
